a4b3e7c3084840738eb5cfa9a98c34477c3e5d57
Mccartin, Blake <blake.mccartin@accenture.com>
2018-07-02 13:50:23 -0500

    vote dogs

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -81,8 +81,8 @@ def index():
 
             # Insert vote result into DB
             vote = request.form['vote']
-            # r.incr('Dogs',1)
-            r.incr(vote,1)
+            r.incr('Dogs',1)
+            # r.incr(vote,1)
             
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
